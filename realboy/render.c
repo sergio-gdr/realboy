f885fa31b21ca6cb1311a117817a28ead08d2461
Sergio Gómez <sergio.g.delreal@gmail.com>
2025-12-27 13:30:15 -0500

    render: add boilerplate for pixman renderer
    Signed-off-by: Sergio Gómez <sergio.g.delreal@gmail.com>

--- realboy/render.c
+++ realboy/render.c
@@ -16,6 +16,14 @@
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
  */
 
+#define _GNU_SOURCE
+
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/mman.h>
+#include <unistd.h>
+
 #include <pixman.h>
 
 #include "render.h"
@@ -30,8 +38,40 @@ typedef struct {
 render_t render;
 
 void render_fini() {
+	munmap(render.data, render.framebuffer.size * sizeof(uint32_t));
+	close(render.framebuffer_fd);
 }
 
 int render_init() {
+	render.framebuffer.width = 160*4;
+	render.framebuffer.height = 144*4;
+	render.framebuffer.stride = render.framebuffer.width * (32 / 8);
+	render.framebuffer.size = render.framebuffer.stride * render.framebuffer.height;
+	render.framebuffer_fd = memfd_create("realboy-bg", MFD_CLOEXEC);
+	if (render.framebuffer_fd == -1) {
+		perror("memfd_create()");
+		return -1;
+	}
+
+	struct framebuffer *framebuf = &render.framebuffer;
+	if (posix_fallocate(render.framebuffer_fd, 0, framebuf->size * sizeof(uint32_t)) != 0) {
+		perror("posix_fallocate()");
+		goto err;
+	}
+	render.data = mmap(NULL, framebuf->size * sizeof(uint32_t), PROT_READ | PROT_WRITE, MAP_SHARED,
+		render.framebuffer_fd, 0);
+	if (render.data == MAP_FAILED) {
+		perror("mmap()");
+		goto err;
+	}
+	void *src = malloc(framebuf->size * sizeof(uint32_t));
+	if (!src) {
+		perror("malloc()");
+		goto err;
+	}
+
 	return 0;
+err:
+	render_fini();
+	return -1;
 }
